c496badd368db6325f8edb1deda2c03c0887d33e
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2018-10-14 19:36:43 +0300

    аплоадер рисует картинки

--- resources/frontend/components/uploader.vue
+++ resources/frontend/components/uploader.vue
@@ -1,24 +1,33 @@
 <template lang="pug">
   .uploader
     label.uploader__elem(
-      :class="{active: isActiveView}"
-      @dragover="isActiveView = true"
-      @dragleave="isActiveView = false"
+    :class="{active: isActiveView, uploaded: anyPicsUploaded}"
+      @dragover.prevent="isActiveView = true"
+      @dragleave.prevent="isActionView = false"
+      @drop.prevent="handleUpload"
     )
       input(
         type="file"
         multiple="true"
+        ref="uploader"
+        v-if="anyPicsUploaded === false"
       ).uploader__real
       .uploader__items
-        uploader-item(v-for="n in 10")
-      .uploader__desc 
+        uploader-item(
+          v-for="pic in picsToRender"
+          :pic="pic"
+          :key="pic.id"
+        )
+      .uploader__desc(v-if="anyPicsUploaded === false") 
         span.uploader__desc-text Перетащите фото сюда или #[a.uploader__link выберите файл]
 
 </template>
 
 <script lang="ts">
+import uuid from "uuid/v1";
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
+import { renderFile } from "../helpers/files";
 import uploaderItem from "./uploaderItem.vue";
 
 @Component({
@@ -27,6 +36,54 @@ import uploaderItem from "./uploaderItem.vue";
 })
 export default class Uploader extends Vue {
   public isActiveView: boolean = false;
+
+  public picsToRender: object[] = [];
+
+  get anyPicsUploaded() {
+    return this.picsToRender.length !== 0;
+  }
+  public handleUpload(e): void {
+    e.preventDefault();
+    this.isActiveView = false;
+    this.renderUploadedFiles(e);
+  }
+
+  public renderUploadedFiles(e): void {
+    const uploader = this.$refs.uploader as HTMLElement;
+    const files = e.dataTransfer.files;
+
+    for (const currentFile of files) {
+      this.drawPictures(currentFile).then((reader) => {
+        console.log(reader);
+
+        const picData = {
+          id: uuid(),
+          url: reader.result
+        };
+
+        console.log(picData);
+
+        this.picsToRender.push(picData);
+      });
+    }
+  }
+
+  public drawPictures(file): Promise<any> {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    return new Promise((resolve, reject) => {
+      reader.onloadend = () => {
+        resolve(reader);
+      };
+      reader.onerror = (e) => {
+        throw new Error("error");
+      };
+    });
+    // reader.onloadend = () => {
+    //   whereToRender.style.backgroundImage = `url(${reader.result})`;
+    // };
+  }
 }
 </script>
 
--- resources/frontend/components/uploaderItem.vue
+++ resources/frontend/components/uploaderItem.vue
@@ -1,14 +1,21 @@
 <template lang="pug">
-  .uploader-item
-    .uploader-item__pic Картинка
+  .uploader-item(
+    :style="{backgroundImage: `url('${pic.url}'`}"
+  )
     .uploader-item__close
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 import { renderFile } from "../helpers/files";
-export default class UploaderItem extends Vue {
 
+@Component({
+  name: "UploadItem"
+})
+export default class UploaderItem extends Vue {
+  @Prop({ default: {} })
+  public pic!: string;
 }
 </script>
 
